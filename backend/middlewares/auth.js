1862bad47bef3bdafea7d2e11ce46d529c1ce99d
Oleg <cherbot@mail.ru>
2022-08-23 12:00:54 +0300

    console log for auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,6 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
+    console.log(authorization);
     next(new UnauthorizedError('Необходима авторизация 1'));
     return;
   }
