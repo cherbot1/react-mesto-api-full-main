1f50f6e99de958a44c4f48146455d5de17f61a53
Oleg <cherbot@mail.ru>
2022-08-26 15:14:36 +0300

    token

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -22,7 +22,7 @@ module.exports.login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
-      res.status(OK).send({ jwt: token });
+      res.status(OK).send({ token });
     })
     .catch(next);
 };
